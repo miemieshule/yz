a5473253ef1ae9602d91d9b7ead5edea91699330
miemieshule <miemieshule@126.com>
2021-05-26 11:13:07 +0800

    js 第一次作业

---
+++ JS/littlejs.js
@@ -0,0 +1,24 @@
+
+// 弹出姓名a
+var sanme = '杨嘉栋 ' + '小金鱼' +'大金鱼' ;
+
+var sanme1 = '杨嘉栋' ;
+var sanme2 = '杨大炮' ;
+var sanme3 = '杨坚果' ;
+
+//弹出姓名 b
+alert('name' + sanme);
+
+// 弹出姓名a
+alert('names' + sanme1);
+alert('names' + sanme2);
+alert('names' + sanme3);
+
+
+
+// 测试变量是否能打印传出
+function ts (params) {
+    var a = '高手' ;
+    alert('a');
+    console.log(a);
+}   
