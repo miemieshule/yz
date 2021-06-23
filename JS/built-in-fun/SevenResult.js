ac0b476f5bf26730271b81e52d15b1245febaa1c
miemieshule <miemieshule@126.com>
2021-06-23 09:51:48 +0800

    ES进阶: 闭包

--- JS/built-in-fun/SevenResult.js
+++ JS/built-in-fun/SevenResult.js
@@ -2,96 +2,95 @@
 
 // 作业
 // 使用JavaScript内置字符串函数，处理 “‘源栈’：飞哥小班教学，线下免费收看” ：将“飞哥”改成“大神”，“线下”改成“线上”。
-// 思路 
-// String.prototype.replace()  匹配一个替换部分 
 // slogan 口号 、change  改 
-// var Asloan = "源栈’：飞哥小班教学，线下免费收看".replace('飞哥','大神');
-// var Bsloan = Asloan.replace('线下','线上')
+// var onesloan = "源栈’：飞哥小班教学，线下免费收看".replace('飞哥','大神');
+// var twosloan = Asloan.replace('线下','线上')
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 作业
 // 将数组['why','gIT','vs2019','community','VERSION']规范化，让所有元素：
 // 首字母大写开头，其他字母小写
 // 截去超过6个字符的部分，如'community'将变成'Commun'
 
-// 我知道的
-// 字母大写内置函数 数组下标 字符串下标  
-// 思路1
-// a测试
-// 获取数组下标0值 ，从0值中取到第一个字母， \\可以 \\可以 
-// 获取第一个字母 资料
+// 资料
+// 获取第一个字母
 // var anyString = "Mozilla";
 // console.log(anyString.substring(0,1));
 
-// 思路2 截取超过六个字符的 
-// b测试
-// 循环获得数组中每个字符串，并且截取六位，存入新数组 
-
-// 结合
-// 外层一个循环，一个vs2019，  if 判断长度是否大于6，如果大于6 截取，拿到第一个字母，然后大写方法，传入新数组 
-// 小于六 拿第一个字母 放大  ,
-// 最大的问题 各个方法嵌套使用的不熟 
-
-// 资料 
 // 大写  b.toUpperCase();
-// "DA"
 // var YJDD = YJD.substring(YJD.indexOf('d'));
-//  转换 小写 toLowerCase()
 
-// 第一版 
-// issting();
-// function issting(params) {
-//     var stingdata = ['community','VERSION','commu'];
-//     var sb = [];
-//     for (let i = 0; i < stingdata.length ; i++) {
-//         if (stingdata[i].length > 6 ) {
-//           var Atemp =  stingdata[i].substring(1,6) ;
-//           var Btemp =   stingdata[i].substring(0,1) ;
-//           var Ctemp = Btemp.toUpperCase() + Atemp ;
+// 小写 toLowerCase()
+
+// var userdata = ['why','gIT','vs2019','community','VERSION'];
+// stingfun(userdata);
+// function stingfun(params) {
+
+//     var newarry = [];
+//     for (let i = 0; i < params.length ; i++) {
+//         if (params[i].length > 6 ) {
+//           var onetemp =  params[i].substring(0,6) ;
+//           var twotemp =   onetemp.substring(0,1).toUpperCase() + onetemp.substring(1,6).toLowerCase() ;
+//           newarry.push(twotemp)
 //         }else{
-//           stingdata[i].substring(0,1).toUpperCase() + stingdata[i].substring(1,6).toLowerCase()  ;
+//          var threetemp = params[i].substring(0,1).toUpperCase() + params[i].substring(1,6).toLowerCase()  ;
+//          newarry.push(threetemp)
 //         }
 //     }
-// }
-
-// 第二版 
-// issting();
-// function issting(params) {
-//     var stingdata = ['why','gIT','vs2019','community','VERSION'];
-//     var newstingdata = [];
-//     for (let i = 0; i < stingdata.length ; i++) {
-//         if (stingdata[i].length > 6 ) {
-//           var Atemp =  stingdata[i].substring(0,6) ;
-//           var Btemp =   Atemp.substring(0,1).toUpperCase() + Atemp.substring(1,6).toLowerCase() ;
-//           newstingdata.push(Btemp)
-//         }else{
-//          var Ctemp = stingdata[i].substring(0,1).toUpperCase() + stingdata[i].substring(1,6).toLowerCase()  ;
-//          newstingdata.push(Ctemp)
-//         }
-//     }
-//     console.log(newstingdata );
+//     console.log(newarry);
 // }
 // 控制台输出
 // (5) ["Why", "Git", "Vs2019", "Commun", "Versio"]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 作业
 // 创建一个函数getRandomArray(length, max)，能返回一个长度不大于length，每个元素值不大于max的随机整数数组。
 //  空数组 []  长度3 假设max是 4 ，4以内的随机整数 
-// 思路 根据长度生成次数 for if判断max值，生成随机值 
-// var isarray = [];
-// isarray.length = 3 ;
+//  根据长度生成次数 for if判断max值，生成随机值 
 
-// getRandomArray(isarray , 4)
+// var nullarray = [];
+// nullarray.length = 3 ;
+
+// getRandomArray(nullarray , 4)
 
 // function getRandomArray(length, max) {
-//     var Bisarray = [];
+//     var radomarray = [];
 //     for (let i = 0; i < length.length; i++) {
 //         if (i>=0) {
 //          var radom =  Math.floor(Math.random()*max);
-//             Bisarray.push(radom);
+//             radomarray.push(radom);
 //         }
         
 //     }
+//     console.log(radomarray);
 // }
+
+
+
 
 // 作业
 // 生成一个函数toChinese()，可将传入的日期参数（如：new Date()）转换成中文日期格式（如：2019年10月4日 16点54分）
@@ -153,8 +152,6 @@
 // 时间戳转数据字符串，
 // 数字字符串转时间戳
 
-// 总结
-// 还是有好多陌生的玩意 
 
 // 正则 作业  // 是否是合格的Email格式
 // 使用正则表达式判断某个字符串:
@@ -178,18 +175,6 @@
 // https://blog.csdn.net/lanseguhui/article/details/81325818
 
 
-// 是否是小数（正负小数都可以）
-// figure
-// figure(2);
-// function figure(params) {
-//         var isfigure = 14;
-//     var reg = /^(([^0][0-9]+|0)\.([0-9]{1,2}))$/;
-//     if(reg.test(isfigure )){
-//         alert("是小数");
-//     }else{
-//         alert("不是小数");
-//     }
-// }
 
 // 将所有以zyf-开头的属性去掉zyf- （尽可能多的制造测试用例，比如：<a lzyf-old=''，或者：<span>zyf---+---fyz</span> ……）ssname(2);
 // 失败 不搞了 
