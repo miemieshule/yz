d2166fccd18528d7f841a7837bfe6834d0dc769e
miemieshule <miemieshule@126.com>
2021-07-02 15:20:44 +0800

    Vue：{{ data }}：模板 / 绑定 / 渲染 / 响应式  作业

---
+++ vue/VueHome.js
@@ -0,0 +1,6 @@
+var vm = new Vue({
+    el: "#logo",
+    data: {
+        StudyLogo: "键盘敲烂 代码过万"
+    }
+})
---
+++ vue/article-770.js
@@ -0,0 +1,8 @@
+var listContent  = new Vue({
+    el: "#div-content",
+    data: {
+        time: "021年04月25日 17点48分1秒",
+        head:"常见面试题：GET和POST",
+        img:'<img src="/img/wzxqa2.png" alt="">',
+    }
+})
