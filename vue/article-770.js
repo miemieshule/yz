b81b4a831bdfcf73f3cb9d60313515f737571fbd
miemieshule <miemieshule@126.com>
2021-07-03 17:28:10 +0800

    Vue：条件/列表渲染：v-if-else / v-show / v-for 作业

--- vue/article-770.js
+++ vue/article-770.js
@@ -4,5 +4,32 @@ var listContent  = new Vue({
         time: "021年04月25日 17点48分1秒",
         head:"常见面试题：GET和POST",
         img:'<img src="/img/wzxqa2.png" alt="">',
+
+        loginState: true ,
+        isAuthor:false,
+        isAdmin:true,
+
     }
 })
+
+//  登录
+// 控制台输出 已登录 显示输入框 true  未登录 listContent.isAuthor = false
+
+// 作者
+//  是作者 listContent.isAuthor = true 
+//  不是作者 举报者  listContent.isAuthor = fales       
+//  不是作者 管理员  listContent.isAuthor = false, listContent.isAdmin = true
+
+
+// 免费广告列表图标颜色 渲染修改 
+var rightContent  = new Vue({
+    el: "#right-content",
+    data: {
+        iscolors:["0.2","0.4","0.6","0.8",] 
+    }
+})
+
+// 评论列表：含评论的回复 
+// 看不懂 
+
+
