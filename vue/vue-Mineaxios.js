2f7f12aeab2d3753c9b3b3b5390c28215b5153a5
miemieshule <miemieshule@126.com>
2021-07-17 11:04:28 +0800

    axios 作业 注册页进行邀请人与邀请码检查

--- vue/vue-Mineaxios.js
+++ vue/vue-Mineaxios.js
@@ -1,4 +1,87 @@
 
+// news:消息
+Mock.mock("/Student/23", {
+    "yjdNewNews": 'true' ,
+    })
+
+
+    // 计时器容器 
+var RefreshContainer ;
+// 刷新 container：容器  存放是否有新消息请求  
+var  blinkfunContainer ;
+// 计时器容器 存放每秒的闪烁 
+
+var newNewsfun  =  new Vue({   // news 消息 
+el:'#new-News',
+data:{
+    newNews : '1'  ,
+    color :'red',  //行内样式绑定颜色
+    nowUser:[  //now 现在用户
+        { 
+            user:{ name : '杨嘉栋' , role:'common'}  // role 角色  //common:普通
+        },
+    ],
+   
+},
+
+methods: {
+    //  blink:闪烁功能  
+    blinkfun: function () {
+        blinkfunContainer = setInterval(function () {
+            if (this.color == 'red') {
+                this.color = 'blue';
+            } else {
+                this.color = 'red';
+            }
+        }.bind(this), 3000)    //每隔一秒变换一次颜色
+    },
+
+
+    //  Refresh:刷新、Examine：检查 News：消息
+    RefreshExamineNews:function () {
+        RefreshContainer  =  setInterval(function () {
+
+            // 第一次请求 
+            axios.request({
+                url: "/Student/23",
+                method:"get",
+                params:{ 
+                    // nowUser:this.nowUser ,  //传送用户信息 
+                }
+            }).then((response) => {  
+                console.log('1');
+                if (response.data.yjdNewNews === "true") {
+                    this.blinkfun();
+                }else{
+                }
+
+                // 第一次请求成功后,发起第二次请求 
+                axios.request({
+                    url: "/Student/23",
+                    method:"get",
+                    params:{
+                        // nowUser:this.nowUser ,  //传送用户信息 
+                    }
+                }).then((response) => {  
+                    console.log(2);
+                    if (response.data.yjdNewNews == "true") {
+                        clearTimeout(RefreshContainer); // 取消请求 
+                    }else{
+                        console.log('没有新消息');
+                    }
+                })
+
+            })
+        }.bind(this), 9000)    //每隔4秒请求一次 
+
+    } ,
+
+
+},
+
+})
+
+newNewsfun.RefreshExamineNews()
 
 
 
@@ -7,28 +90,30 @@
 
 
 // news:消息
-Mock.mock("/Student/23", {
+Mock.mock("/Student/24", {
     // unread:未读 news:消息 
     "yjdNews":  '@ctitle()' ,
-    "yjdNewNews": 'false' ,
     "unreadNews":
     [
         {
-            'title': '@ctitle()',//时间 
+            'articleTime':'@datetime',//时间 
             'unreadContent' : '@cparagraph(1, 1)',  //段落
             'Readstate':'', // read:阅读状态 
+            'Selectstate':'', //选择状态 
 
         },
         {
-            'title': '@ctitle()',//时间 
+            'articleTime':'@datetime',//时间 
             'unreadContent' : '@cparagraph(1, 1)',  //段落
             'Readstate':'', // read:阅读状态 
+            'Selectstate':'', //选择状态 
 
         },
         {
-            'title': '@ctitle()',//时间 
+            'articleTime':'@datetime',//时间 
             'unreadContent' : '@cparagraph(1, 1)',  //段落
             'Readstate':'', // read:阅读状态 
+            'Selectstate':'', //选择状态 
 
         },
 
@@ -41,6 +126,7 @@ var newsListFun  =  new Vue({   // news 消息列表功能
 el:'#news-ListFun',
 data:{
     allUnread:'',
+    allCheckbox:false , //全选 复选框 
     nowUser:[  //now 现在用户
         { 
             user:{ name : '杨嘉栋' , role:'common'}  // role 角色  //common:普通
@@ -51,7 +137,7 @@ data:{
 
 created:function(){
         axios.request({
-        url: "/Student/23",
+        url: "/Student/24",
         method:"get",
     }).then((response) => {  
         this.allUnread = response.data.unreadNews;
@@ -60,11 +146,66 @@ created:function(){
 },
 
 methods: {
+    // 全选功能 
+    allfun:function(){  //复选框 所有方法 
+        if (!this.allCheckbox) {
+            return this.allUnread.map(function (s,i) {
+                s.Selectstate = true ;
+            })
+
+        }else{
+            return this.allUnread.map(function (s,i) {
+                return s.Selectstate = false;
+            })
+        }
+    },
+
+    // 点击已读 
+    // 状态值   s.Readstate = '' 变为  s.Readstate = '1' 
+    readstatefun:function (params) {
+     this.allUnread.filter(function (s,i) {
+            if (s.Selectstate === true) {
+                s.Readstate = '1'
+            }else{
+            } 
+        }.bind(this))
+
+        // 解放闪烁计时器 
+        clearTimeout(blinkfunContainer);
+
+        // 5秒后 调用方法 已读dom元素传给后端 
+        setTimeout(
+            function () {
+                newsListFun.portalReads();
+        },5000)
+    },
+
+
+    // portal:传送 已读：Reads 
+    portalReads:function (params) {
+    // 过滤出点击已阅读按钮的元素 
+    var  ReadsElement =[]
+   this.allUnread.filter(function (s,i) {
+           if (s.Readstate == '1') {
+            ReadsElement.push(s)
+           }else{
+           } 
+        }.bind(this))
+
+        // 获取元素传给后端 
+        axios.request({
+        url: "/Student/23",
+        method:"get",
+        params:{ 
+            ReadsElement,
+        }
+    })
+
+    },
 
 },
 
 })
-
 
 
 
--- vue/vue-register.js
+++ vue/vue-register.js
@@ -52,3 +52,74 @@
     // 关闭页面 拿到值 判断 失败 / 输入框没值 也会弹出 else块内不执行 
 
 
+// request() / then() / catch() / 文件上传 / 简写 / 全局 / 拦截器 作业 
+// 在注册页面，完成检查：
+// 用户名已使用
+// 邀请人不存在，或邀请码错误的检测
+
+
+Mock.mock("/Student/24", {
+    
+    // Check:检查 End:结束 邀请用户名 
+    "CheckEndInviteName":  'true' ,
+    "CheckEndInviteNumber":'fales',
+    })
+
+// Check:检查 input:输入 
+var Checkinput = new Vue({  //表单绑定 
+    el: "#Check-input",
+    data: {
+        invitename: '',  //invite:邀请名 
+        invitenumber: '',  //invite:邀请码 
+        invitenamehint:false, //hint:提示 
+        invitenumberhint: false,  //invite:邀请码提示  
+    },
+
+
+   methods:{
+        // 邀请人姓名 输入框失焦绑定方法 
+        CheckName:function(){
+            axios.request({
+                url: "/Student/24",
+                method:"get",
+                params:{ 
+                    // invitename:this.invitename ,
+                },
+            }).then((response) => {  
+                if (response.data.CheckEndInviteName === "true") {
+                    this.invitenamehint = true
+                }else{
+                    this.invitenamehint = false
+
+                }
+         
+            })
+
+        },
+
+        // 邀请码 
+        // 传给后端邀请码数值 
+        // 请求获取后端返回的布尔值 
+        CheckNumber:function(){
+            axios.request({
+                url: "/Student/24",
+                method:"get",
+                params:{ 
+                    // invitenumber:this.invitenumber ,
+                },
+            }).then((response) => {  
+                if (response.data.CheckEndInviteNumber === "true") {
+                    this.invitenumberhint = true
+                }else{
+                    this.invitenumberhint = false
+
+                }
+         
+            })
+
+        },
+
+  
+
+    },
+})
